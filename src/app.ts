87f1e7125c82d8cca67645f3d4fb40216f1ceb7b
Angus Lee <zangus.lee@DFIretailgroup.com>
2024-03-19 09:50:25 -0400

    Add the URL encoder

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,9 @@ const app = express();
 // MIDDLEWARE for format the request body
 app.use(express.json({limit: "10kb"}));
 
+// URL Encoder
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+
 // Setup the CORS
 app.use(cors({
     origin: "https://angusproductmanagement.netlify.app/",
