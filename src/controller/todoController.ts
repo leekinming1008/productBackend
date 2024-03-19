c3970ca9c1e3705b5915b8ee3efdf33c3a54cc7e
Angus Lee <zangus.lee@DFIretailgroup.com>
2024-03-18 21:20:15 -0400

    Update the controller functions

--- src/controller/todoController.ts
+++ src/controller/todoController.ts
@@ -1,44 +1,31 @@
-// Data
-// const todos = [{id: 1, title: "coding"},{id: 2, title: "cleaning"}];
-import plane from "../models/planeModel";
+import  Plane  from "../models/planeModel";
 
 export const getAllPlanes = async (_req: any, res: any) => {
-    res.status(200).send(await plane.find());
+    res.status(200).send(await Plane.find());
 }
 
 export const createNewPlane = async(req: any, res: any) => {
-    const response = await plane.create(req.body);
+    const response = await Plane.create(req.body);
     res.status(201).json(response);
 }
 
 export const getPlane = async (req: any, res: any) => {
     const {id} = req.params;
-    const response = await plane.findById(id);
+    const response = await Plane.findById(id);
     res.status(201).json(response);
 }
 
-// export const deleteProduct = (req: any, res: any) => {
-//     const {id} = req.params;
-//     const deleteItemIndex = todos.findIndex((obj) => obj.id.toString() == id)
-//     console.log("delete item index: " + deleteItemIndex)
-//     if (deleteItemIndex != -1) {
-//         todos.splice(deleteItemIndex, 1);
-//         res.status(201).send("Successfully delete the todo item")
-//     } else {
-//         res.status(204).send("Cannot found the delete item in the database");
-//     }
+export const deleteProduct = async (req: any, res: any) => {
+    const {id} = req.params;
+    const response = await Plane.deleteOne({_id: id});
+    res.status(202).json(response);
     
-// }
+}
 
-// export const editProduct = (req: any, res: any)=> {
-//     const {id} = req.params;
-//     const {title} = req.body;
-//     const editItemIndex = todos.findIndex((obj)=> obj.id.toString() == id)
-//     if (editItemIndex != -1) {
-//         todos[editItemIndex].title = title;
-//         res.status(201).send("Successfully edit the todo item")
-//     } else {
-//         res.status(204).send("Cannot find the edit item id in the database.")
-//     }
-    
-//}
+export const editProduct = async (req: any, res: any)=> {
+    const {id} = req.params;
+    const editplane = req.body;
+
+    const response = await Plane.updateOne({_id: id}, editplane);
+    res.status(201).json(response);
+}
--- src/routes/todoRoutes.ts
+++ src/routes/todoRoutes.ts
@@ -1,11 +1,10 @@
-import app from "../app"
 import express from "express"
-import {getAllPlanes, createNewPlane, getPlane} from "../controller/todoController"
+import {getAllPlanes, createNewPlane, getPlane, deleteProduct, editProduct} from "../controller/todoController"
 
 const router = express.Router();
 
 router.route("/").get(getAllPlanes).post(createNewPlane);
-router.route("/:id").get(getPlane);
+router.route("/:id").get(getPlane).delete(deleteProduct).put(editProduct);
 
 
 // get to the home page
