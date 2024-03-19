253bf1645344aed60495611a9aa685930278ae7a
Angus Lee <zangus.lee@DFIretailgroup.com>
2024-03-18 21:26:10 -0400

    Add the try catch block for each function

--- src/controller/todoController.ts
+++ src/controller/todoController.ts
@@ -1,31 +1,57 @@
 import  Plane  from "../models/planeModel";
 
 export const getAllPlanes = async (_req: any, res: any) => {
-    res.status(200).send(await Plane.find());
-}
-
-export const createNewPlane = async(req: any, res: any) => {
-    const response = await Plane.create(req.body);
-    res.status(201).json(response);
-}
-
-export const getPlane = async (req: any, res: any) => {
-    const {id} = req.params;
-    const response = await Plane.findById(id);
-    res.status(201).json(response);
-}
-
-export const deleteProduct = async (req: any, res: any) => {
-    const {id} = req.params;
-    const response = await Plane.deleteOne({_id: id});
-    res.status(202).json(response);
+    try {
+        res.status(200).send(await Plane.find());
+    } catch (error) {
+        res.status(500).send("Error catch when calling the get all planes function.")
+        console.log("Error catch when calling the get all planes function.", error);
+    }
     
 }
 
-export const editProduct = async (req: any, res: any)=> {
-    const {id} = req.params;
-    const editplane = req.body;
+export const createNewPlane = async(req: any, res: any) => {
+    try{
+        const response = await Plane.create(req.body);
+        res.status(201).json(response);
+    }
+    catch (error) {
+        res.status(500).send("Error catch when calling the create new planes function.")
+        console.log("Error catch when calling the create new planes function.", error);
+    }
+    
+}
 
-    const response = await Plane.updateOne({_id: id}, editplane);
-    res.status(201).json(response);
+export const getPlane = async (req: any, res: any) => {
+    try {
+        const {id} = req.params;
+        const response = await Plane.findById(id);
+        res.status(201).json(response);
+    } catch (error) {
+        res.status(500).send("Error catch when calling the get planes function.")
+        console.log("Error catch when calling the get planes function.", error);
+    }
+}
+
+export const deleteProduct = async (req: any, res: any) => {
+    try {
+        const {id} = req.params;
+        const response = await Plane.deleteOne({_id: id});
+        res.status(202).json(response);
+    } catch (error) {
+        res.status(500).send("Error catch when calling the delete planes function.")
+        console.log("Error catch when calling the delete planes function.", error);
+    }   
+}
+
+export const editProduct = async (req: any, res: any)=> {
+    try {
+        const {id} = req.params;
+        const editplane = req.body;
+        const response = await Plane.updateOne({_id: id}, editplane);
+        res.status(201).json(response);
+    } catch (error) {
+        res.status(500).send("Error catch when calling the edit planes function.")
+        console.log("Error catch when calling the edit planes function.", error);
+    }
 }
