35a3e82f313e5f11daab76159b33d1492517baff
Angus Lee <zangus.lee@DFIretailgroup.com>
2024-03-19 09:47:58 -0400

    setup the CORS

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
 import express from "express";
 import router from "./routes/todoRoutes";
+import cors from "cors";
 
 const app = express();
 
 // MIDDLEWARE for format the request body
 app.use(express.json({limit: "10kb"}));
+
+// Setup the CORS
+app.use(cors({
+    origin: "https://angusproductmanagement.netlify.app/",
+}))
 
 // Routing
 app.use("/planes", router)
