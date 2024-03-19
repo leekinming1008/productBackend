dd9613455cb61ded52e8d9e6fdddd4e8713dc048
Angus Lee <zangus.lee@DFIretailgroup.com>
2024-03-19 18:35:58 -0400

    Update cors allow list

--- src/app.ts
+++ src/app.ts
@@ -11,12 +11,19 @@ app.use(express.json({limit: "10kb"}));
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 
 // Setup the CORS
+var allowlist = ["http://localhost:5174", "https://angusproductmanagement.netlify.app/"];
+
 app.use(cors({
-    origin: "http://localhost:5174",
+    origin: "http://localhost:5174, https://angusproductmanagement.netlify.app/",
 }))
 
 // Routing
 app.use("/planes", router)
+
+// get to the home page
+app.get("/", (req, res) => {
+    res.status(200).send("Welcome to Our Express Server");
+})
 
 export default app;
 
--- src/routes/todoRoutes.ts
+++ src/routes/todoRoutes.ts
@@ -6,10 +6,4 @@ const router = express.Router();
 router.route("/").get(getAllPlanes).post(createNewPlane);
 router.route("/:id").get(getPlane).delete(deleteProduct).put(editProduct);
 
-
-// get to the home page
-// app.get("/", (req, res) => {
-//     res.status(200).send("Welcome to Our Express Server");
-// })
-
 export default router
