2980342a0bcf6b1393e90637377b8522ef1fb614
Angus Lee <zangus.lee@DFIretailgroup.com>
2024-03-19 18:40:44 -0400

    Update the CORS

--- src/app.ts
+++ src/app.ts
@@ -11,11 +11,21 @@ app.use(express.json({limit: "10kb"}));
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 
 // Setup the CORS
-var allowlist = ["http://localhost:5174", "https://angusproductmanagement.netlify.app/"];
+var allowlist = ["http://localhost:5174", "https://angusproductmanagement.netlify.app"];
+var corsOptionsDelegate = function (req: any, callback: any) {
+  var corsOptions;
+  if (allowlist.indexOf(req.header("Origin")) !== -1) {
+    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+  }
+  callback(null, corsOptions); // callback expects two parameters: error and options
+};
+app.use(cors(corsOptionsDelegate));
 
-app.use(cors({
-    origin: "http://localhost:5174, https://angusproductmanagement.netlify.app/",
-}))
+// app.use(cors({
+//     origin: "http://localhost:5174, https://angusproductmanagement.netlify.app/",
+// }))
 
 // Routing
 app.use("/planes", router)
